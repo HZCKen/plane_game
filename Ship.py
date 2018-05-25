c7178074616bc62c2401874a7daeeea074dbb4cd
huangzhicheng <13826457912@163.com>
2018-05-25 11:00:58 +0800

    发射子弹，飞船移动提取

--- Ship.py
+++ Ship.py
@@ -1,81 +1,52 @@
 import pygame
-import sys
-from enum import Enum
-
-
-class MoveDirection(Enum):
-    stop = 0
-    up = 1
-    down = 2
-    left = 3
-    right = 4
 
 
 class Ship(object):
-    def __init__(self, screen):
+    def __init__(self, screen, ai_setting):
         self.screen = screen
         self.icon_imgae = pygame.image.load("images/ship.bmp")
         self.rect = self.icon_imgae.get_rect()
         self.screen_rect = self.screen.get_rect()
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom - 10
-        self.__direction = MoveDirection.stop
+
+        self.is_move_left = False
+        self.is_move_right = False
+        self.is_move_up = False
+        self.is_move_down = False
+        self.ai_setting = ai_setting
+
 
     # 显示飞船
     def show_ship(self):
         self.screen.blit(self.icon_imgae, self.rect)
-
-    # 飞船监听事件移动
-    def move_event(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_a or event.key == pygame.K_LEFT:
-                    self.__direction = MoveDirection.left
-                elif event.key == pygame.K_d or event.key == pygame.K_RIGHT:
-                    self.__direction = MoveDirection.right
-                elif event.key == pygame.K_w or event.key == pygame.K_UP:
-                    self.__direction = MoveDirection.up
-                elif event.key == pygame.K_s or event.key == pygame.K_DOWN:
-                    self.__direction = MoveDirection.down
-                elif event.key == pygame.K_q:
-                    sys.exit()
-            elif event.type == pygame.KEYUP:
-                self.__direction = MoveDirection.stop
-        self.__move()
+    # 更新飞船位置
+    def update(self):
+        if self.is_move_left:
+            self.__move_left()
+        elif self.is_move_right:
+            self.__move_right()
+        elif self.is_move_up:
+            self.__move_up()
+        elif self.is_move_down:
+            self.__move_down()
 
     # 向左
     def __move_left(self):
-        self.rect.centerx -= 5
-        if self.rect.centerx - (self.rect.width / 2) <= 0:
-            self.rect.centerx = self.rect.width / 2
+        if self.rect.left > 0:
+            self.rect.centerx -= self.ai_setting.ship_speed_factor
 
     # 向右
     def __move_right(self):
-        self.rect.centerx += 5
-        if self.rect.centerx + (self.rect.width / 2) >= self.screen_rect.width:
-            self.rect.centerx = self.screen_rect.width - (self.rect.width / 2)
+        if self.rect.right < self.screen_rect.right:
+            self.rect.centerx += self.ai_setting.ship_speed_factor
 
     # 向上
     def __move_up(self):
-        self.rect.bottom -= 5
-        if self.rect.bottom - self.rect.height <= 0:
-            self.rect.bottom = self.rect.height
+        if self.rect.top > self.screen_rect.top + self.ai_setting.ship_speed_factor:
+            self.rect.top -= self.ai_setting.ship_speed_factor
 
     # 向下
     def __move_down(self):
-        self.rect.bottom += 5
-        if self.rect.bottom + (self.rect.height / 2) >= self.screen_rect.height:
-            self.rect.bottom = self.screen_rect.bottom
-
-    # 飞船移动
-    def __move(self):
-        if self.__direction == MoveDirection.up:
-            self.__move_up()
-        elif self.__direction == MoveDirection.down:
-            self.__move_down()
-        elif self.__direction == MoveDirection.left:
-            self.__move_left()
-        elif self.__direction == MoveDirection.right:
-            self.__move_right()
+        if self.rect.bottom < self.screen_rect.bottom:
+            self.rect.bottom += self.ai_setting.ship_speed_factor
--- setting.py
+++ setting.py
@@ -4,3 +4,10 @@ class Setting(object):
         self.screen_width = 480
         self.screen_height = 800
         self.backgrounColor = (230, 230, 230)
+
+        self.ship_speed_factor = 5
+
+        self.bullet_speed_factor = 7
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
